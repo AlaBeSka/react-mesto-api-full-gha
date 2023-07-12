d0cddfddda19525fab289e4a70fe93b9985cdc0c
AlaBeSka <ehanbegov@yandex.ru>
2023-07-12 21:41:24 +0300

    fix secret key

--- backend/cfg.js
+++ backend/cfg.js
@@ -1,4 +1,4 @@
-const SECRET_SIGNING_KEY = process.env;
+const { SECRET_SIGNING_KEY = 'dev-secret' } = process.env;
 const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 module.exports = {
