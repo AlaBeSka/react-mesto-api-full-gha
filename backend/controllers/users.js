978e51db4a24e6a1c7fc8df237531c4a736609e6
AlaBeSka <ehanbegov@yandex.ru>
2023-07-14 15:13:23 +0300

    fix: испарвлена ршибка автотестов

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,21 +1,21 @@
-const { ValidationError } = require('mongoose').Error;
-const { CastError } = require('mongoose').Error;
-const User = require('../models/user');
-const NotFoundErr = require('../errors/notFound');
-const BadRequestErr = require('../errors/badReq');
+const { ValidationError } = require("mongoose").Error;
+const { CastError } = require("mongoose").Error;
+const User = require("../models/user");
+const NotFoundErr = require("../errors/notFound");
+const BadRequestErr = require("../errors/badReq");
 
 module.exports.getUsers = (req, res, next) => {
-  User
-    .find({})
+  User.find({})
     .then((users) => res.send({ users }))
     .catch(next);
 };
-const findUserById = (id) => User.findById(id).then((user) => {
-  if (user) {
-    return user;
-  }
-  throw new NotFoundErr('Пользователь c указанным _id не найден');
-});
+const findUserById = (id) =>
+  User.findById(id).then((user) => {
+    if (user) {
+      return user;
+    }
+    throw new NotFoundErr("Пользователь c указанным _id не найден");
+  });
 
 module.exports.getUserId = (req, res, next) => {
   const { id } = req.params;
@@ -26,8 +26,8 @@ module.exports.getUserId = (req, res, next) => {
       if (err instanceof CastError) {
         next(
           new BadRequestErr(
-            'Переданы некорректные данные при поиске пользователя',
-          ),
+            "Переданы некорректные данные при поиске пользователя"
+          )
         );
       } else {
         next(err);
@@ -43,20 +43,27 @@ module.exports.getCurrentUserInfo = (req, res, next) => {
     .catch(next);
 };
 
-const updateUserProfileData = (userId, data) => User.findByIdAndUpdate(userId, data, {
-  new: true,
-  runValidators: true,
-}).then((user) => {
-  if (user) {
-    return user;
-  }
-  throw new NotFoundErr('Пользователь с указанным _id не найден');
-}).catch((err) => {
-  if (err instanceof ValidationError) {
-    return next(new BadRequestErr('Переданы некорректные данные при обновлении профиля'));
-  }
-  return next(err);
-});
+const updateUserProfileData = (userId, data, next) =>
+  User.findByIdAndUpdate(userId, data, {
+    new: true,
+    runValidators: true,
+  })
+    .then((user) => {
+      if (user) {
+        return user;
+      }
+      throw new NotFoundErr("Пользователь с указанным _id не найден");
+    })
+    .catch((err) => {
+      if (err instanceof ValidationError) {
+        return next(
+          new BadRequestErr(
+            "Переданы некорректные данные при обновлении профиля"
+          )
+        );
+      }
+      return next(err);
+    });
 
 module.exports.updateUserProfile = (req, res, next) => {
   const { name, about } = req.body;
