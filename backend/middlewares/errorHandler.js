a20ed325483a97dc15b060bc23015e57940de77e
AlaBeSka <ehanbegov@yandex.ru>
2023-07-15 16:08:49 +0300

    вернул errorHandler как был

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,15 +1,7 @@
 const errorHandler = (err, _, res, next) => {
   const statusCode = err.statusCode || 500;
 
-  let message = '';
-  if (statusCode === 500) {
-    message = 'На сервере произошла ошибка';
-  } else if (statusCode === 401) {
-    message = 'Неправильные почта или пароль';
-  } else {
-    message = err.message;
-  }
-
+  const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
   res.status(statusCode).send({ message });
   next();
 };
