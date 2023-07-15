b7d4f6441c73650f8e0e57d675b10b571c9598e6
AlaBeSka <ehanbegov@yandex.ru>
2023-07-15 15:54:07 +0300

    исправления автотестов1

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,7 +1,15 @@
 const errorHandler = (err, _, res, next) => {
   const statusCode = err.statusCode || 500;
 
-  const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
+  let message = '';
+  if (statusCode === 500) {
+    message = 'На сервере произошла ошибка';
+  } else if (statusCode === 401) {
+    message = 'Неправильные почта или пароль';
+  } else {
+    message = err.message;
+  }
+
   res.status(statusCode).send({ message });
   next();
 };
